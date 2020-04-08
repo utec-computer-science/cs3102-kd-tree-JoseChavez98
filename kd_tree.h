6497d797dea73c418df344299e35807ad0c59af0
JoseChavez98 <jose.chavez@utec.edu.pe>
2020-04-07 22:12:51 -0500

    feat(init): setting up main functions

---
+++ kd_tree.h
@@ -0,0 +1,22 @@
+#ifndef KD_TREE_H 
+#define KD_TREE_H
+
+class kd_tree
+{
+private:
+    /* data */
+public:
+    kd_tree(/* args */);
+    ~kd_tree();
+};
+
+kd_tree::kd_tree(/* args */)
+{
+}
+
+kd_tree::~kd_tree()
+{
+}
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <climits>
+
+int main(){
+    
+    return 0;
+}
